b29566b24723f23f4e04e9a7b863f5f3287d4f1c
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-09-19 14:00:42 -0300

    Added Layout, Navbar and Footer Components

---
+++ pages/components/Navbar.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <Image
+          src="/images/pokeball.png"
+          width="30"
+          height="30"
+          alt="PokeNext"
+        />
+        <h1>PokeNext</h1>
+      </div>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>Sobre</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
